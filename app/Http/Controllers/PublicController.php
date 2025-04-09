6c4e76759ef80ac969820f7a9a63b629c303c784
DanyaErezer <pavlov.shura12@gmail.com>
2025-04-09 15:19:26 +0700

    Feature: Заполнение PublicController, установка api

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\ProductCategory;
 
 class PublicController extends Controller
 {
-    //
+    public function products()
+    {
+        return Product::paginate(6);
+    }
+
+    public function productBySlug($slug)
+    {
+        return Product::where('slug', $slug)->firstOrFail();
+    }
+
+    public function categoryTree()
+    {
+        return $this->buildTree(ProductCategory::whereNull('parent_id')->get());
+    }
+
+    private function buildTree($categories)
+    {
+        return $categories->map(function ($category) {
+            return [
+                'id' => $category->id,
+                'title' => $category->title,
+                'children' => $this->buildTree($category->children)
+            ];
+        });
+    }
+
+    public function categoriesWithProducts()
+    {
+        return ProductCategory::with('products')->get();
+    }
 }
